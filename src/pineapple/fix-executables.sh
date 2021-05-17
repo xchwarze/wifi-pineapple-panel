26f83a1d89a82f13f6a333a9010ec3eef0bf36c7
DSR! <xchwarze@gmail.com>
2021-05-17 16:54:44 -0300

    Panel - Add script for fix executables

---
+++ src/pineapple/fix-executables.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+chmod +x /etc/init.d/pineapd
+chmod +x /etc/uci-defaults/93-pineap.sh
+chmod +x /pineapple/modules/Advanced/formatSD/format_sd
+chmod +x /pineapple/modules/Help/files/debug
+chmod +x /pineapple/modules/PineAP/executable/executable
+chmod +x /pineapple/modules/Reporting/files/reporting
