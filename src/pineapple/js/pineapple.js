6476e8899b01cce9bc6916fee2182c9dd4875819
DSR! <xchwarze@gmail.com>
2022-10-08 03:50:02 -0300

    Exprose Pineapple API in JS window

--- src/pineapple/js/pineapple.js
+++ src/pineapple/js/pineapple.js
@@ -2,30 +2,32 @@
     var pineapple = angular.module('pineapple', ['ngRoute', 'ngCookies'])
 
     .config(['$routeProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide', function($routeProvider, $controllerProvider, $compileProvider, $filterProvider, $provide) {
-        
         pineapple.controllerProvider = $controllerProvider;
         pineapple.compileProvider    = $compileProvider;
         pineapple.routeProvider      = $routeProvider;
         pineapple.filterProvider     = $filterProvider;
         pineapple.provide            = $provide;
     }])
+
     .run(['$api', function($api){
+        window.pineapple = $api;
+
         pineapple.routeProvider
-        .when('/modules/:moduleName', {
-            templateUrl: function(params) {
-                return 'modules/'+ params.moduleName +'/module.html';
-            },
-            controller: function() {
-                resizeModuleContent();
-            },
-            resolve: {
-                jsLoader: ['$route', function($route) {
-                    return $.getScript('modules/'+ $route.current.params.moduleName +'/js/module.js');
-                }]
-            }
-        })
-        .otherwise({
-            redirectTo: '/modules/Dashboard'
-        });
+            .when('/modules/:moduleName', {
+                templateUrl: function(params) {
+                    return 'modules/'+ params.moduleName +'/module.html';
+                },
+                controller: function() {
+                    resizeModuleContent();
+                },
+                resolve: {
+                    jsLoader: ['$route', function($route) {
+                        return $.getScript('modules/'+ $route.current.params.moduleName +'/js/module.js');
+                    }]
+                }
+            })
+            .otherwise({
+                redirectTo: '/modules/Dashboard'
+            });
     }])
 })();
